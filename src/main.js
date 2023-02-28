8f78cb3ea396af8cb5283ce8639b187544dd175c
jonglsy <jonelsy@qq.com>
2023-02-28 20:36:23 +0800

    用户管理半成品

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ axios.defaults.baseURL = 'http://43.143.189.51:9999';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem("token");
 Vue.use(VueRouter);
 Vue.use(ElementUI);
-
+import formatDate from './components/DataChange/FormData.js'
+Vue.prototype.$formatDate = formatDate
 import 'element-ui/lib/theme-chalk/index.css';
 const router = new VueRouter({routes: routerMap})
 
